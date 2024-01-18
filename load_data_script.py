e13930826d208ca4228b574bb74a4b2f087783b4
Ustimova <u.juliana.serg@gmail.com>
2024-01-18 22:18:58 +0200

    deploy fix 4

--- load_data_script.py
+++ load_data_script.py
@@ -7,9 +7,16 @@ def load_data():
     with open('data.json', 'r') as file:
         data = json.load(file)
 
-        for app_name, items in data.items():
-            for model_name, model_items in items.items():
-                Model = apps.get_model(app_name, model_name)
+        for app_data in data:
+            app_name = app_data.get("app_name")
+            if not app_name:
+                continue
+
+            for model_name, model_items in app_data.get("items", {}).items():
+                if app_name == "coffee":
+                    Model = apps.get_model("coffee", model_name)
+                elif app_name == "main_app":
+                    Model = apps.get_model("main_app", model_name)
 
                 for item in model_items:
                     try:
